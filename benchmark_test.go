922e321aea643a396fec1f023fc9c024f7835d4b
Timo Savola <timo.savola@iki.fi>
2015-07-08 00:09:27 +0300

    JSON benchmark for comparison

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,6 +1,7 @@
 package python_test
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
@@ -30,7 +31,7 @@ func init() {
 }
 
 func BenchmarkPythonOnly(b *testing.B) {
-	f, err := pyModule.Call("benchmark_factory", b.N, foo, bar, baz)
+	f, err := pyModule.Call("benchmark_python_only_factory", b.N, foo, bar, baz)
 	if err != nil {
 		b.Fatal(err)
 	}
@@ -56,6 +57,45 @@ func BenchmarkGoPython(b *testing.B) {
 		if _, err := f.Invoke(foo, bar, baz); err != nil {
 			b.Fatal(err)
 		}
+	}
+
+	b.StopTimer()
+}
+
+func BenchmarkJSON(b *testing.B) {
+	args := struct{
+		Foo string
+		Bar string
+		Baz int
+	}{
+		Foo: foo,
+		Bar: bar,
+		Baz: baz,
+	}
+
+	resultData := []byte("true")
+
+	f, err := pyModule.Call("benchmark_json_factory", b.N, foo, bar, baz)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		if _, err := json.Marshal(&args); err != nil {
+			b.Fatal(err)
+		}
+
+		var result bool
+
+		if err := json.Unmarshal(resultData, &result); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	if _, err = f.Invoke(); err != nil {
+		b.Fatal(err)
 	}
 
 	b.StopTimer()
--- benchmark_test.py
+++ benchmark_test.py
@@ -1,9 +1,23 @@
-def benchmark_factory(n, foo, bar, baz):
+import json
+
+def benchmark_python_only_factory(n, foo, bar, baz):
 	r = xrange(n)
 
 	def benchmark():
 		for _ in r:
 			function(foo, bar, baz)
+
+	return benchmark
+
+def benchmark_json_factory(n, foo, bar, baz):
+	r = xrange(n)
+	args_data = json.dumps(dict(foo=foo, bar=bar, baz=baz))
+
+	def benchmark():
+		for _ in r:
+			args = json.loads(args_data)
+			result = function(args["foo"], args["bar"], args["baz"])
+			json.dumps(result)
 
 	return benchmark
 
